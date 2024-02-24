8e5bceba367ea426ecf3d3b6f846529261dc2da6
Vladimir <soldatchenkovvolodya@gmail.com>
2024-02-24 18:36:44 +0500

    profil js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 let button = document.querySelector(".button");
 let name = document.querySelector(".name p");
-let name_id = document.querySelector(".name-id");
+let nameid = document.querySelector(".name input");
+let familuy = document.querySelector(".familuy p");
+let familuyid = document.querySelector(".familuy input");
+let number = document.querySelector(".number p ");
+let numberid = document.querySelector(".number input");
+let mail = document.querySelector(".mail p");
+let mailid = document.querySelector(".mail input");
+let city = document.querySelector(".city p ");
+let cityid = document.querySelector(".city input");
 
 button.addEventListener("click", function () {
-  name.innerHTML = "Имя: " + name_id.value;
+  name.innerHTML = "Имя: " + nameid.value;
+  familuy.innerHTML = "Фамилия: " + familuyid.value;
+  number.innerHTML = "Номер: " + numberid.value;
+  mail.innerHTML = "Почта: " + mailid.value;
+  city.innerHTML = "Город: " + cityid.value;
 });
