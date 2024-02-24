cc61ce1b76d7d2dca25e941bd827c6db8f1484c3
Vladimir <soldatchenkovvolodya@gmail.com>
2024-02-24 18:29:20 +0500

    profil css finish

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let button = document.querySelector(".button");
+let name = document.querySelector(".name p");
+let name_id = document.querySelector(".name-id");
 
 button.addEventListener("click", function () {
-  alert("+");
+  name.innerHTML = "Имя: " + name_id.value;
 });
