d19211ad7918e79ab219e6d5522765cde265d4c3
Vladimir <soldatchenkovvolodya@gmail.com>
2024-02-24 18:56:20 +0500

    cookie

--- script.js
+++ script.js
@@ -10,10 +10,31 @@ let mailid = document.querySelector(".mail input");
 let city = document.querySelector(".city p ");
 let cityid = document.querySelector(".city input");
 
+let cookie = false;
+let cookies = document.cookie.split("; ");
+for (let i = 0; i < cookies.length; i += 1) {
+  if (cookies[i].split("=")[0] == "profils_cookie") {
+    cookie = cookies[i].split("=")[1];
+    break;
+  }
+}
+
+if (cookie) {
+  let data = cookie.split("/");
+  name.innerHTML = `<p>${data[0]}</p>`;
+  familuy.innerHTML = `<p>${data[1]}</p>`;
+  number.innerHTML = `<p>${data[2]}</p>`;
+  mail.innerHTML = `<p>${data[3]}</p>`;
+  city.innerHTML = `<p>${data[4]}</p>`;
+}
+
 button.addEventListener("click", function () {
   name.innerHTML = "Имя: " + nameid.value;
   familuy.innerHTML = "Фамилия: " + familuyid.value;
   number.innerHTML = "Номер: " + numberid.value;
   mail.innerHTML = "Почта: " + mailid.value;
   city.innerHTML = "Город: " + cityid.value;
+
+  let new_cookie = `profils_cookie=${name.innerHTML}/${familuy.innerHTML}/${number.innerHTML}/${mail.innerHTML}/${city.innerHTML}; max-age=10000000000`;
+  document.cookie = new_cookie;
 });
